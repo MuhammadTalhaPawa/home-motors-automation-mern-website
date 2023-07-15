50cf0e38103e165cb2918698c8749a715733e386
MuhammadTalhaPawa <133965264+MuhammadTalhaPawa@users.noreply.github.com>
2023-07-15 10:33:03 +0500

    added the get all value function

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ port = process.env.PORT || 8000;
 mongoose.set('strictQuery',false)
 mongoose.connect(
   process.env.atlasMongoURL,
+  // process.env.localMongoURL,
   {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -60,6 +61,43 @@ app.get('/getValue/:myData', async (req, res) => {
   }
 
   res.send({functionType : "getValue", taskCompleted: isTaskCompleted, value: value});
+});
+
+app.get('/getAllValues', async (req, res) => {
+  // jsonKeyValue = await getKeyValue(req.params.myData);
+  // console.log(jsonKeyValue)
+  
+  d = await autoData.find({})
+  // console.log(d);
+
+  var allData = [];
+  for (var i = 0; i < d.length; i++) {
+    var jsonData = {};
+    jsonData.variable = d[i].name;
+    jsonData.value = d[i].value;
+    allData.push(jsonData);
+  }
+
+  var isTaskCompleted = false;
+  if(d.length > 0) {
+    isTaskCompleted = true;
+  }
+  // console.log(allData);
+
+  // var isTaskCompleted = null
+  // var value = null
+
+  // if(d == null) {
+  //   console.log("not found")
+  //   isTaskCompleted = false 
+  //   value = -1 
+  // }else {
+  //   console.log("found")
+  //   isTaskCompleted = true
+  //   value = d.value
+  // }
+
+  res.send({functionType : "getAllValue", taskCompleted: isTaskCompleted, value: allData});
 });
 
 app.get('/insertOne/:myData', async (req, res) => {
