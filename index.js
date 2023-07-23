3da0875d69f6cbf53230e71f69c414812d003590
MuhammadTalhaPawa <133965264+MuhammadTalhaPawa@users.noreply.github.com>
2023-07-23 18:17:00 +0500

    Inserted the update alll values function

--- index.js
+++ index.js
@@ -1,73 +1,92 @@
 require("dotenv").config();
- 
-const express = require('express')
+
+const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 port = process.env.PORT || 8000;
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery", false);
 mongoose.connect(
   process.env.atlasMongoURL,
   // process.env.localMongoURL,
   {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
 );
 
 const autoDataSchema = new mongoose.Schema({
   name: String,
-  value: Number
+  value: Number,
 });
 
-const autoData = mongoose.model('autoData', autoDataSchema);
+const autoData = mongoose.model("autoData", autoDataSchema);
 
 function getKeyValue(data) {
-  var keyValue = data.split('=');
+  var keyValue = data.split("=");
   jsonValue = {
-    key : keyValue[0],
-    value : parseInt(keyValue[1])
-  }
-  return jsonValue
+    key: keyValue[0],
+    value: parseInt(keyValue[1]),
+  };
+  return jsonValue;
 }
 
-app.get('/', (req, res) => {
-  res.send('Home motor automation Panga is not changA...');
+function getAllKeyValues(data) {
+  var things = data.split(",");
+  var keyValueThings = [];
+
+  things.forEach((thing) => {
+    var keyValue = thing.split("=");
+    var obj = {
+      variable: keyValue[0],
+      value: keyValue[1],
+    };
+    keyValueThings.push(obj);
+  });
+
+  return keyValueThings;
+}
+
+app.get("/", (req, res) => {
+  res.send("Home motor update: updateAllValues");
 });
 
-
-app.get('/getValue/:myData', async (req, res) => {
+app.get("/getValue/:myData", async (req, res) => {
   jsonKeyValue = await getKeyValue(req.params.myData);
-  console.log(jsonKeyValue)
-  
-  d = await autoData.findOne({name: jsonKeyValue.key})
+  console.log(jsonKeyValue);
 
-  var isTaskCompleted = null
-  var value = null
+  d = await autoData.findOne({ name: jsonKeyValue.key });
 
-  if(d == null) {
-    console.log("not found")
-    isTaskCompleted = false 
-    value = -1 
-  }else {
-    console.log("found")
-    isTaskCompleted = true
-    value = d.value
+  var isTaskCompleted = null;
+  var value = null;
+
+  if (d == null) {
+    console.log("not found");
+    isTaskCompleted = false;
+    value = -1;
+  } else {
+    console.log("found");
+    isTaskCompleted = true;
+    value = d.value;
   }
 
-  res.send({functionType : "getValue", taskCompleted: isTaskCompleted, value: value});
+  res.send({
+    functionType: "getValue",
+    taskCompleted: isTaskCompleted,
+    value: value,
+  });
 });
 
-app.get('/getAllValues', async (req, res) => {
+app.get("/getAllValues", async (req, res) => {
   // jsonKeyValue = await getKeyValue(req.params.myData);
   // console.log(jsonKeyValue)
-  
-  d = await autoData.find({})
+
+  d = await autoData.find({});
   // console.log(d);
 
   var allData = [];
@@ -79,7 +98,7 @@ app.get('/getAllValues', async (req, res) => {
   }
 
   var isTaskCompleted = false;
-  if(d.length > 0) {
+  if (d.length > 0) {
     isTaskCompleted = true;
   }
   // console.log(allData);
@@ -89,83 +108,119 @@ app.get('/getAllValues', async (req, res) => {
 
   // if(d == null) {
   //   console.log("not found")
-  //   isTaskCompleted = false 
-  //   value = -1 
+  //   isTaskCompleted = false
+  //   value = -1
   // }else {
   //   console.log("found")
   //   isTaskCompleted = true
   //   value = d.value
   // }
 
-  res.send({functionType : "getAllValue", taskCompleted: isTaskCompleted, value: allData});
+  res.send({
+    functionType: "getAllValue",
+    taskCompleted: isTaskCompleted,
+    value: allData,
+  });
 });
 
-app.get('/insertOne/:myData', async (req, res) => {
+app.get("/insertOne/:myData", async (req, res) => {
   jsonKeyValue = await getKeyValue(req.params.myData);
-  console.log(jsonKeyValue)
-  
-  d = await autoData.findOne({name: jsonKeyValue.key})
+  console.log(jsonKeyValue);
 
-  var isTaskCompleted = null
-  if(d == null) {
-    console.log("not found")
+  d = await autoData.findOne({ name: jsonKeyValue.key });
+
+  var isTaskCompleted = null;
+  if (d == null) {
+    console.log("not found");
     await autoData.insertMany({
       name: jsonKeyValue.key,
-      value: jsonKeyValue.value
-    })
-    isTaskCompleted = true    
-  }else {
-    console.log("found")
-    isTaskCompleted = false
+      value: jsonKeyValue.value,
+    });
+    isTaskCompleted = true;
+  } else {
+    console.log("found");
+    isTaskCompleted = false;
   }
 
-  res.send({functionType : "insertOne", taskCompleted: isTaskCompleted});
+  res.send({ functionType: "insertOne", taskCompleted: isTaskCompleted });
 });
 
-app.get('/deleteOne/:myData', async (req, res) => {
+app.get("/deleteOne/:myData", async (req, res) => {
   jsonKeyValue = await getKeyValue(req.params.myData);
-  console.log(jsonKeyValue)
+  console.log(jsonKeyValue);
 
-  d = await autoData.findOne({name: jsonKeyValue.key})
-  
-  var isTaskCompleted = null
-  if(d == null) {
-    console.log("not found")
-    isTaskCompleted = false    
-  }else {
-    console.log("found")
-    await autoData.deleteOne({name:jsonKeyValue.key})
-    isTaskCompleted = true
+  d = await autoData.findOne({ name: jsonKeyValue.key });
+
+  var isTaskCompleted = null;
+  if (d == null) {
+    console.log("not found");
+    isTaskCompleted = false;
+  } else {
+    console.log("found");
+    await autoData.deleteOne({ name: jsonKeyValue.key });
+    isTaskCompleted = true;
   }
 
-  res.send({functionType : "deleteOne", taskCompleted: isTaskCompleted});
+  res.send({ functionType: "deleteOne", taskCompleted: isTaskCompleted });
 });
 
-app.get('/deleteAll', async (req, res) => {
-  await autoData.deleteMany({}) 
+app.get("/deleteAll", async (req, res) => {
+  await autoData.deleteMany({});
 
-  res.send({functionType : "deleteAll", taskCompleted: true});
+  res.send({ functionType: "deleteAll", taskCompleted: true });
 });
 
-app.get('/updateOne/:myData', async(req, res) => {
+app.get("/updateOne/:myData", async (req, res) => {
   jsonKeyValue = await getKeyValue(req.params.myData);
-  console.log(jsonKeyValue)
-  
-  d = await autoData.findOne({name: jsonKeyValue.key})
+  console.log(jsonKeyValue);
 
-  var isTaskCompleted = null
-  if(d == null) {
-    console.log("not found")
-    isTaskCompleted = false    
-  }else {
-    console.log("found")
-    await autoData.updateOne({name: jsonKeyValue.key},{$set : {value: jsonKeyValue.value}})
-    isTaskCompleted = true
+  d = await autoData.findOne({ name: jsonKeyValue.key });
+
+  var isTaskCompleted = null;
+  if (d == null) {
+    console.log("not found");
+    isTaskCompleted = false;
+  } else {
+    console.log("found");
+    await autoData.updateOne(
+      { name: jsonKeyValue.key },
+      { $set: { value: jsonKeyValue.value } }
+    );
+    isTaskCompleted = true;
   }
 
-  res.send({functionType : "updateOne", taskCompleted: isTaskCompleted});
+  res.send({ functionType: "updateOne", taskCompleted: isTaskCompleted });
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening on ${port} `),
-);
+app.get("/updateAll/:myData", async (req, res) => {
+  allKeyValues = getAllKeyValues(req.params.myData);
+  // console.log(allKeyValues);
+
+  for (let i = 0; i < allKeyValues.length; i++) {
+    keyValue = allKeyValues[i];
+    console.log(keyValue);
+    f = await autoData.findOne({ name: keyValue.variable });
+    if (f == null) {
+      console.log("Not Found: inserting: ");
+      await autoData.insertMany({
+        name: keyValue.variable,
+        value: keyValue.value,
+      });
+    } else {
+      console.log("found: updating");
+      await autoData.updateOne(
+        { name: keyValue.variable },
+        { $set: { value: keyValue.value } }
+      );
+    }
+  }
+
+  // allKeyValues.forEach((keyvalue) => {
+  //   f = await autoData.findOne({ name: keyvalue.variable });
+  //   console.log(keyvalue);
+  // });
+
+  res.send({ functionType: "updateAll", taskCompleted: true });
+});
+
+app.listen(port, () => console.log(`Example app listening on ${port} `));
